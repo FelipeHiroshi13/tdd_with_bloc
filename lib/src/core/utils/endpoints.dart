7b306206f440477f76b4ba765995dcef0872a478
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2023-10-29 17:09:21 -0400

    feature: add http in remote data source

---
+++ lib/src/core/utils/endpoints.dart
@@ -0,0 +1,8 @@
+abstract class Endpoints {
+  static String kBaseUrl =
+      'https://653c1364d5d6790f5ec7cf8e.mockapi.io/test-api';
+
+  // * USERS
+  static const createUser = '/users';
+  static const getUsersEndpoint = '/user';
+}
--- lib/src/features/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/src/features/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import '../../../../core/utils/endpoints.dart';
 import '../models/user_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class IAuthenticationRemoteDataSource {
   Future<void> createUser({
@@ -7,4 +11,24 @@ abstract class IAuthenticationRemoteDataSource {
   });
 
   Future<List<UserModel>> getUsers();
+}
+
+class AuthenticationRemoteDataSource
+    implements IAuthenticationRemoteDataSource {
+  AuthenticationRemoteDataSource({required this.client});
+
+  final http.Client client;
+
+  @override
+  Future<void> createUser({required String name, required String avata}) async {
+    await client.post(
+        Uri.parse('${Endpoints.kBaseUrl}/${Endpoints.createUser}'),
+        body: jsonEncode({'name': name, 'avata': avata}));
+  }
+
+  @override
+  Future<List<UserModel>> getUsers() {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
 }
