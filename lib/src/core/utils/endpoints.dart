ab555ad24b37c78218aedc75e9cc2d74169a852b
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2023-10-29 18:02:07 -0400

    feature: getUsers from remote data source

--- lib/src/core/utils/endpoints.dart
+++ lib/src/core/utils/endpoints.dart
@@ -1,8 +1,7 @@
 abstract class Endpoints {
-  static String kBaseUrl =
-      'https://653c1364d5d6790f5ec7cf8e.mockapi.io/test-api';
+  static String kBaseUrl = '653c1364d5d6790f5ec7cf8e.mockapi.io';
 
   // * USERS
-  static const createUser = '/users';
-  static const getUsersEndpoint = '/user';
+  static const createUser = 'test-api/users';
+  static const getUsersEndpoint = 'test-api/user';
 }
--- lib/src/features/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/src/features/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:dummy_app_with_bloc/src/core/errors/exceptions.dart';
+
 import '../../../../core/utils/endpoints.dart';
 import '../models/user_model.dart';
 import 'package:http/http.dart' as http;
@@ -21,14 +23,40 @@ class AuthenticationRemoteDataSource
 
   @override
   Future<void> createUser({required String name, required String avata}) async {
-    await client.post(
-        Uri.parse('${Endpoints.kBaseUrl}/${Endpoints.createUser}'),
-        body: jsonEncode({'name': name, 'avata': avata}));
+    try {
+      final response = await client.post(
+          Uri.https(Endpoints.kBaseUrl, Endpoints.createUser),
+          body: jsonEncode({'name': name, 'avata': avata}));
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw APIException(
+            message: response.body, statusCode: response.statusCode);
+      }
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 
   @override
-  Future<List<UserModel>> getUsers() {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+  Future<List<UserModel>> getUsers() async {
+    try {
+      final response = await client
+          .get(Uri.https(Endpoints.kBaseUrl, Endpoints.getUsersEndpoint));
+
+      if (response.statusCode != 200) {
+        throw APIException(
+            message: response.body, statusCode: response.statusCode);
+      }
+
+      List<dynamic> users = jsonDecode(response.body);
+
+      return users.map((e) => UserModel.fromMap(e)).toList();
+    } on APIException {
+      rethrow;
+    } catch (e) {
+      throw APIException(message: e.toString(), statusCode: 505);
+    }
   }
 }
