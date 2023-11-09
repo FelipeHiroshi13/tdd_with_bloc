37b4064535421e87c86c06c076452f45864e173a
--global <--global>
2023-11-08 21:13:48 -0400

    test: authentication cubit

--- test/src/features/authentication/presentation/cubit/authentication_cubit_test.dart
+++ test/src/features/authentication/presentation/cubit/authentication_cubit_test.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:dummy_app_with_bloc/src/features/authentication/domain/usecases/create_user.dart';
 import 'package:dummy_app_with_bloc/src/features/authentication/domain/usecases/get_users.dart';
 import 'package:dummy_app_with_bloc/src/features/authentication/presentation/cubit/authentication_cubit.dart';
@@ -13,16 +15,41 @@ void main() {
   late CreateUser createUser;
   late AuthenticationCubit cubit;
 
+  const tCreateUserParams = CreateUserParams(avatar: 'avatar', name: 'name');
+
   setUp(() {
     getUsers = MockGetUser();
     createUser = MockCreateUser();
     cubit = AuthenticationCubit(
         createUserUseCase: createUser, getUsersUseCase: getUsers);
+    registerFallbackValue(tCreateUserParams);
   });
 
   tearDown(() => cubit.close());
 
   test('Initial state should be [AuthenticationInitial]', () async {
     expect(cubit.state, const AuthenticationInitial());
+  });
+
+  group('Create user', () {
+    blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [CreatingUser, UserCreated] when sucessful',
+        build: () {
+          when(() => createUser(any()))
+              .thenAnswer((_) async => const Right(null));
+          return cubit;
+        },
+        act: (cubit) => cubit.createUser(
+              name: tCreateUserParams.name,
+              avatar: tCreateUserParams.avatar,
+            ),
+        expect: () => const [
+              CreatingUser(),
+              UserCreated(),
+            ],
+        verify: (_) {
+          verify(() => createUser(tCreateUserParams)).called(1);
+          verifyNoMoreInteractions(createUser);
+        });
   });
 }
