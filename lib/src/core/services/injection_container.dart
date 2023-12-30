aedc07dc476074526f65f4ea2d87f17fdc9b82f3
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2023-12-30 18:50:16 -0400

    refactor: removed comments and TODOs

--- lib/src/core/services/injection_container.dart
+++ lib/src/core/services/injection_container.dart
@@ -29,28 +29,3 @@ Future<void> init() async {
         () => AuthenticationRemoteDataSource(client: sl()))
     ..registerLazySingleton(() => http.Client());
 }
-
-
-// Future<void> init() async {
-//   sl
-//     // App Logic
-//     ..registerFactory(() => AuthenticationCubit(
-//           createUser: sl(),
-//           getUsers: sl(),
-//         ))
-
-//     // Use cases
-//     ..registerLazySingleton(() => CreateUser(sl()))
-//     ..registerLazySingleton(() => GetUsers(sl()))
-
-//     // Repositories
-//     ..registerLazySingleton<AuthenticationRepository>(
-//         () => AuthenticationRepositoryImplementation(sl()))
-
-//     // Data Sources
-//     ..registerLazySingleton<AuthenticationRemoteDataSource>(
-//         () => AuthRemoteDataSrcImpl(sl()))
-
-//     // External Dependencies
-//     ..registerLazySingleton(http.Client.new);
-// }
